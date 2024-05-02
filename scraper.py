966b8d9695d37f3ddbfb19282613a0c1d9f6a1b0
Azmayen Fayek Sabil <77103994+AzmayenSabil@users.noreply.github.com>
2024-05-02 12:48:40 +0600

    updated

--- scraper.py
+++ scraper.py
@@ -15,28 +15,34 @@ def fetch_posts_and_comments(subreddit_name):
     total_posts_fetched = 0
     rows_added_to_csv = 0
 
-    with open('reddit_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    with open('reddit_data_2.csv', 'w', newline='', encoding='utf-8') as csvfile:
         fieldnames = ['post_id', 'post_title', 'comment_id', 'comment']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
 
-        for submission in subreddit.hot(limit=100):  # Adjust limit as needed
+        for submission in subreddit.hot(limit=20):  # Adjust limit as needed
             total_posts_fetched += 1
             post_id = submission.id
             post_title = submission.title
             submission.comments.replace_more(limit=None)
+            comment_count = 0
             for comment in submission.comments.list():
+                if comment_count >= 12:
+                    break
                 comment_id = comment.id
                 comment_body = comment.body
                 writer.writerow(
                     {'post_id': post_id, 'post_title': post_title, 'comment_id': comment_id, 'comment': comment_body})
                 rows_added_to_csv += 1
+                comment_count += 1
 
     return total_posts_fetched, rows_added_to_csv
 
 
 if __name__ == "__main__":
-    total_posts, rows_added = fetch_posts_and_comments('IsraelPalestine')
+    # total_posts, rows_added = fetch_posts_and_comments('IsraelPalestine')
+    total_posts, rows_added = fetch_posts_and_comments('UkraineRussiaReport')
+
     print(f"Total number of posts fetched: {total_posts}")
     print(f"Number of rows added to CSV: {rows_added}")
     print("Data has been successfully fetched and stored in reddit_data.csv.")
